721d21707f3827a3169f9f65fb8d1f7ae1657409
implodingduck <zsek320@gmail.com>
2025-06-10 13:25:19 -0500

    interacting with metered billing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # graph-metered-billing
+
+## Resources
+* https://learn.microsoft.com/en-us/graph/cloud-communication-online-meeting-application-access-policy#configure-application-access-policy
---
+++ scripts/export-channel-messages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source .env
+
+# Get an Entra ID token for the Microsoft Graph API
+TOKEN=$(curl -s -X POST \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "client_id=${AZURE_CLIENT_ID}" \
+  -d "scope=https://graph.microsoft.com/.default" \
+  -d "client_secret=${AZURE_CLIENT_SECRET}" \
+  -d "grant_type=client_credentials" \
+  "https://login.microsoftonline.com/${AZURE_TENANT_ID}/oauth2/v2.0/token" | jq -r '.access_token')
+# Check if the token was retrieved successfully
+if [ -z "$TOKEN" ]; then
+  echo "Failed to retrieve access token."
+  exit 1
+fi
+
+# Get the channel messages from the Microsoft Graph API
+curl -s -X GET \
+  -H "Authorization: Bearer ${TOKEN}" \
+  "https://graph.microsoft.com/v1.0/teams/${TEAM_ID}/channels/${CHANNEL_ID}/messages" | jq '.'
